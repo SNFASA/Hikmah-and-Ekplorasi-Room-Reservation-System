501c333bb59e2e6366ffc2e79e744ac012dfbb79
SNFASA <syednabil.0413@gmail.com>
2025-01-14 21:56:42 +0800

    file

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User; // Ensure this is the correct model
+use App\Models\User; 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
