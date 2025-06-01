25a8ddfeed6331e9d9a3b8e304607d9e91f3ca49
SNFASA <syednabil.0413@gmail.com>
2025-06-01 18:24:09 +0800

    update

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -26,12 +26,12 @@
                 <div class="col-lg-2 col-md-6 col-12">
                     <h6 class="fw-bold">UTHM Links</h6>
                     <ul class="list-unstyled">
-                        <li><a href="https://www.uthm.edu.my/en/"><i class="fas fa-link me-2 text-primary"></i> UTHM Website</li></a>
-                        <li><a href="https://penerbit.uthm.edu.my/ojs/"><i class="fas fa-link me-2 text-primary"></i> UTHM Journals</li></a>
-                        <li><a href="https://publisher.uthm.edu.my/periodicals/"><i class="fas fa-link me-2 text-primary"></i> UTHM Periodicals</li></a>
-                        <li><a href="http://e-bookstore.uthm.edu.my/"><i class="fas fa-link me-2 text-primary"></i> UTHM e-Bookstore</li></a>
-                        <li><a href="https://news.uthm.edu.my/ms/e-wacana/"><i class="fas fa-link me-2 text-primary"></i> UTHM e-News</li></a>
-                        <li><a href="https://wecare.uthm.edu.my/"><i class="fas fa-link me-2 text-primary"></i> UTHM WeCare</li></a>
+                        <li><a href="https://www.uthm.edu.my/en/"><i class="fas fa-link me-2 text-white"></i> UTHM Website</li></a>
+                        <li><a href="https://penerbit.uthm.edu.my/ojs/"><i class="fas fa-link me-2 text-white"></i> UTHM Journals</li></a>
+                        <li><a href="https://publisher.uthm.edu.my/periodicals/"><i class="fas fa-link me-2 text-white"></i> UTHM Periodicals</li></a>
+                        <li><a href="http://e-bookstore.uthm.edu.my/"><i class="fas fa-link me-2 text-white"></i> UTHM e-Bookstore</li></a>
+                        <li><a href="https://news.uthm.edu.my/ms/e-wacana/"><i class="fas fa-link me-2 text-white"></i> UTHM e-News</li></a>
+                        <li><a href="https://wecare.uthm.edu.my/"><i class="fas fa-link me-2 text-white"></i> UTHM WeCare</li></a>
                     </ul>
                 </div>
 
@@ -39,11 +39,11 @@
                 <div class="col-lg-3 col-md-6 col-12">
                     <h6 class="fw-bold">Related Links</h6>
                     <ul class="list-unstyled">
-                        <li><a href="http://perpun.upm.edu.my/myuninet/"><i class="fas fa-link me-2 text-primary"></i> My UniNet</li></a>
-                        <li><a href="https://www.pnm.gov.my/"><i class="fas fa-link me-2 text-primary"></i> National Library of Malaysia</li></a>
-                        <li><a href="https://www.u-library.gov.my/portal/web/guest/home"><i class="fas fa-link me-2 text-primary"></i> u-Pustaka</li></a>
-                        <li><a href="https://kik.pnm.gov.my/index.php/en/"><i class="fas fa-link me-2 text-primary"></i> Katalog Induk Kebangsaan (KIK)</li></a>
-                        <li><a href="https://ppaj.johor.gov.my/"><i class="fas fa-link me-2 text-primary"></i> Perbadanan Perpustakaan Awam Johor</li></a>
+                        <li><a href="http://perpun.upm.edu.my/myuninet/"><i class="fas fa-link me-2 text-white"></i> My UniNet</li></a>
+                        <li><a href="https://www.pnm.gov.my/"><i class="fas fa-link me-2 text-white"></i> National Library of Malaysia</li></a>
+                        <li><a href="https://www.u-library.gov.my/portal/web/guest/home"><i class="fas fa-link me-2 text-white"></i> u-Pustaka</li></a>
+                        <li><a href="https://kik.pnm.gov.my/index.php/en/"><i class="fas fa-link me-2 text-white"></i> Katalog Induk Kebangsaan (KIK)</li></a>
+                        <li><a href="https://ppaj.johor.gov.my/"><i class="fas fa-link me-2 text-white"></i> Perbadanan Perpustakaan Awam Johor</li></a>
                     </ul>
                 </div>
 
@@ -88,7 +88,7 @@
     text-decoration: none;
 }
 .footer a:hover {
-    color: #0d6efd; /* Bootstrap primary */
+     color: #0d6efd !important;
 }
 
 </style>
